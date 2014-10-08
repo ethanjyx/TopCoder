8f6ae32554a7d44d939d8fde44d1d4919ece4a46
Yixing jiang <ethanjyx@umich.edu>
2014-10-08 19:44:05 -0400

    c nailed down

--- UM_ACM_Team_Selection/c.cpp
+++ UM_ACM_Team_Selection/c.cpp
@@ -11,47 +11,70 @@
 
 using namespace std;
 
-int main() {
-	char* traversed = new char[2000000000]; // origin 1000000000 [0 1]
-	for (int i = 0; i < 2000000000; ++i)
-	{
-		traversed[i] = 'N';
+struct Interval {
+	int start;
+	int end;
+};
+
+struct comp {
+	bool operator() (Interval i1, Interval i2) {
+		if (i1.start == i2.start) {
+			return i1.end > i2.end;
+		}
+		return i1.start < i2.start;
 	}
+} mycomp;
+
+int main() {
 	int N;
-	int cur = 0;
-	int move = 0;
-	int r = 0;
-	int origin = 1000000000;
-	char dir;
 	cin >> N;
+	int cur = 0;
+	int move;
+	char dir;
+	vector<Interval> ivs;
 	while(N != 0) {
 		cin >> move >> dir;
-		if (dir == 'R') {
-			for (int i = 0; i < move; ++i)
-			{
-				if (traversed[origin + cur + i] == 'N') {
-					traversed[origin + cur + i] = 'Y';
-				} else if (traversed[origin + cur + i] == 'Y') {
-					++r;
-					traversed[origin + cur + i] = 'S';
-				}
-			}
-			cur = cur + move;
+		if (dir == 'L') {
+			ivs.push_back(Interval{cur-move, cur});
+			cur -= move;
 		} else {
-			for (int i = 0; i < move; ++i)
-			{
-				if (traversed[origin + cur - i] == 'N') {
-					traversed[origin + cur - i] = 'Y';
-				} else if (traversed[origin + cur - i] == 'Y') {
-					++r;
-					traversed[origin + cur - i] = 'S';
-				}
-			}
-			cur = cur - move;
+			ivs.push_back(Interval{cur, cur+move});
+			cur += move;
 		}
 		--N;
 	}
-	delete[] traversed;
+	sort(ivs.begin(), ivs.end(), mycomp);
+
+	int r = 0;
+	auto itr = ivs.begin();
+	int s = itr->start, e = itr->end;
+	++itr;
+	for(; itr != ivs.end(); ++itr) {
+		if (itr->start > e) {
+			s = itr->start;
+			e = itr->end;
+		} else if(itr->start >= s){
+			if (itr->end <= e) {
+				r += itr->end - itr->start;
+				s = itr->end;
+			} else {
+				r += e - itr->start;
+				s = e;
+				e = itr->end;
+			}
+		} else {
+			if (itr->end < s) {
+				continue;
+			} else if (itr->end <= e) {
+				r += itr->end - s;
+				s = itr->end;
+			} else {
+				r += e - s;
+				s = e;
+				e = itr->end;
+			}
+		}
+	}
 	cout << r << endl;
 	return 0;
 }
